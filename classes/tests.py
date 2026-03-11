93215c7d2a2c922693b9bb8857a817d7a31d5094
Court-Pencil <courtneyhulse97@outlook.com>
2026-03-11 18:34:12 +0000

    test: add failing tests for studioclass model

--- classes/tests.py
+++ classes/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
-from .models import Category
+from .models import Category, StudioClass
 
-class CategoryTestCase(TestCase):
+class CategoryModelTest(TestCase):
 
     def setUp(self):
         self.category = Category.objects.create(
@@ -17,6 +17,29 @@ class CategoryTestCase(TestCase):
         self.assertEqual(self.category.name, "Pottery")   
         self.assertEqual(self.category.slug, "pottery")
         self.assertEqual(self.category.description, "A class for pottery enthusiasts.")
+
+class StudioClassModelTest(TestCase):
+
+    def setUp(self):
+        self.category = Category.objects.create(
+            name="Pottery",
+            slug="pottery",
+            description="A class for pottery enthusiasts."
+        )
+
+    def setUp(self):
+        self.studioclass = StudioClass.objects.create(
+            title = 'Intro to Pottery',
+            category = 'Pottery',
+            instructor = 'Steve',
+            date = '03/03/26',
+            duration = '2 hours',
+            capacity = 10,
+            price = 50.00,
+            image = 'path/to/image.jpg',
+            descritpion = 'Learn the basics of pottery in this introductory class.',
+            is_published = True,
+        )
         
 
 
