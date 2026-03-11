bf05160bff46950d70f11f33b6d9c08123c33f83
Court-Pencil <courtneyhulse97@outlook.com>
2026-03-11 18:39:10 +0000

    feat: implement StudioClass model

--- classes/models.py
+++ classes/models.py
@@ -8,3 +8,17 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class StudioClass(models.Model):
+    title = models.CharField(max_length=100),
+    category = models.CharField(max_length=50),
+    instructor = models.CharField(max_length=100),
+    date = models.CharField(max_length=20),
+    duration = models.CharField(max_length=20),
+    capacity = models.IntegerField(),
+    price = models.DecimalField(max_digits=6, decimal_places=2),
+    image = models.CharField(max_length=255),
+    descritpion = models.TextField(max_length=255, null=True, blank=True),
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
