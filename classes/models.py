dde93a5434ff434378a0ea6ebea2df19344148fd
Court-Pencil <courtneyhulse97@outlook.com>
2026-03-11 13:14:54 +0000

    feat: add description to Category model and tested

--- classes/models.py
+++ classes/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     name= models.CharField(max_length=50)
-    slug= models.SlugField(max_length=250)
+    slug= models.SlugField(max_length=50)
+    description = models.TextField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.name
--- classes/tests.py
+++ classes/tests.py
@@ -7,7 +7,7 @@ class CategoryTestCase(TestCase):
         self.category = Category.objects.create(
             name="Pottery",
             slug="pottery",
-            
+            description="A class for pottery enthusiasts."
         )
 
     def test_category_str_returns_name(self):
@@ -16,7 +16,7 @@ class CategoryTestCase(TestCase):
     def test_category_saves_correctly(self):
         self.assertEqual(self.category.name, "Pottery")   
         self.assertEqual(self.category.slug, "pottery")
-       
+        self.assertEqual(self.category.description, "A class for pottery enthusiasts.")
         
 
 
