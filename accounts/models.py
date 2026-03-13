9f8363a3befb3c4d7aa8dda237c4b46d79b030b9
Court-Pencil <courtneyhulse97@outlook.com>
2026-03-13 14:57:50 +0000

    feat: implement UserProfile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE, related_name='accounts')
+    bio = models.TextField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.user.username
+
