3bf1be4472b2880a786814994b27e93e5b915037
Court-Pencil <courtneyhulse97@outlook.com>
2026-03-16 08:36:19 +0000

    docs: add ERD and data schema to README

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from bookings.models import Booking
-from accounts.models import UserProfile
-from classes.models import Category, StudioClass
+
 
 class BookingAdmin(admin.ModelAdmin):
     list_display = ['user', 'studio_class', 'status', 'stripe_payment_id']
