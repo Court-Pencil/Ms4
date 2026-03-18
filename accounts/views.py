d61875314a52d73fea60ae7703d98525662c1207
Court-Pencil <courtneyhulse97@outlook.com>
2026-03-18 18:40:09 +0000

    feat: add profile view and template

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from accounts.models import UserProfile
 
-# Create your views here.
+@login_required
+def profile(request):
+    profile = UserProfile.objects.get(user=request.user)
+    return render(request, 'accounts/profile.html', {'profile': profile})
