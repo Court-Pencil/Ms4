4d56adf0ab6c0480b4b2b0dc272a95d0e172d5ef
Court-Pencil <courtneyhulse97@outlook.com>
2026-03-18 12:07:23 +0000

    test: add test for anonymous user can access signup page

--- accounts/tests.py
+++ accounts/tests.py
@@ -5,3 +5,7 @@ class AuthViewTest(TestCase):
     def test_anon_can_acess_login_page(self):
         response = self.client.get("/accounts/login/")
         self.assertEqual(200, response.status_code)
+
+    def test_anon_can_acess_login_page(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(200, response.status_code)
