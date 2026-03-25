19cad35105c4f23d55fb3fffc0198460ad869deb
Court-Pencil <courtneyhulse97@outlook.com>
2026-03-25 12:02:34 +0000

    feat: add class detail view, template and URL with slug

--- classes/urls.py
+++ classes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import class_list
+from .views import class_list, class_details
 
 urlpatterns = [
     path('', class_list, name = 'class_list'),
+    path('<slug:slug>/', class_details, name='class_details')
 ]
--- classes/views.py
+++ classes/views.py
@@ -4,3 +4,8 @@ from classes.models import StudioClass
 def class_list(request):
     class_list = StudioClass.objects.all()
     return render(request, 'classes/class_list.html', {'class_list': class_list})
+
+
+def class_details(request, slug):
+    class_details = StudioClass.objects.get(slug=slug)
+    return render(request, 'classes/class_detail.html', {'class_details': class_details})
