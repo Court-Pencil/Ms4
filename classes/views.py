b698e0ccf229d67abb1e845a3bf1dfe64253cb56
Court-Pencil <courtneyhulse97@outlook.com>
2026-03-27 21:22:45 +0000

    feat: add category filter and title search to class list view. add theme.css

--- classes/views.py
+++ classes/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from classes.models import StudioClass
+from classes.models import StudioClass, Category
 
 def class_list(request):
     class_list = StudioClass.objects.all()
-    return render(request, 'classes/class_list.html', {'class_list': class_list})
+    filter_categories = Category.objects.all()
+    class_type = request.GET.get('class_type')
+    if class_type:
+        class_list = class_list.filter(category__slug=class_type)
+    search = request.GET.get('search')
+    if search:
+        filtered_class_list = class_list.filter(title__icontains=search)
+    else:
+        filtered_class_list = class_list
+    return render(request, 'classes/class_list.html', {'class_list': filtered_class_list, 'filter_categories': filter_categories})
 
 
 def class_details(request, slug):
