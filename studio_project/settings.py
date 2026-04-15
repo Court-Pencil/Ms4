052858dc2a89063284ad9a934a354b4ff4bacbdf
Court-Pencil <courtneyhulse97@outlook.com>
2026-04-15 21:13:45 +0100

    feat: configure media files, fix image loading

--- studio_project/settings.py
+++ studio_project/settings.py
@@ -147,6 +147,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
 
--- studio_project/urls.py
+++ studio_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +26,5 @@ urlpatterns = [
     path('userprofile/', include('accounts.urls')),
     path('classes/', include('classes.urls')),
     path('bookings/', include('bookings.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
