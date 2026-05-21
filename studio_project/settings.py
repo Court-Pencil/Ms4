f66e0bfd33e80d320127bea36788c2ef6940d1f0
Court-Pencil <courtneyhulse97@outlook.com>
2026-05-21 15:17:53 +0100

    fix: configure PostgreSQL for Heroku deployment

--- studio_project/settings.py
+++ studio_project/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
+import dj_database_url
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -100,11 +102,12 @@ WSGI_APPLICATION = 'studio_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
 
 
