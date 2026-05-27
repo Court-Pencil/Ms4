0d5ba314d314a0a353e50de8def2ad57a4f34f6f
Court-Pencil <courtneyhulse97@outlook.com>
2026-05-28 00:56:06 +0100

    fix: explicitly configure Cloudinary credentials

--- studio_project/settings.py
+++ studio_project/settings.py
@@ -123,6 +123,13 @@ cloudinary.config(
     cloudinary_url=os.getenv('CLOUDINARY_URL')
 )
 
+import cloudinary
+cloudinary.config(
+    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.getenv('CLOUDINARY_API_KEY'),
+    api_secret=os.getenv('CLOUDINARY_API_SECRET')
+)
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Password validation
