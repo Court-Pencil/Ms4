426907af4bf8679bb775917f15e187d92bb2a4f6
Court-Pencil <courtneyhulse97@outlook.com>
2026-05-28 01:03:25 +0100

    fix: clean up Cloudinary configuration

--- studio_project/settings.py
+++ studio_project/settings.py
@@ -123,12 +123,6 @@ cloudinary.config(
     cloudinary_url=os.getenv('CLOUDINARY_URL')
 )
 
-import cloudinary
-cloudinary.config(
-    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.getenv('CLOUDINARY_API_KEY'),
-    api_secret=os.getenv('CLOUDINARY_API_SECRET')
-)
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
