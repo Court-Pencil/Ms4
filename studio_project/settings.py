2a6e44198a599c4add8052221db089028d1e5d45
Court-Pencil <courtneyhulse97@outlook.com>
2026-07-03 14:01:11 +0100

    fix: remove unnecassary load static and block css

--- studio_project/settings.py
+++ studio_project/settings.py
@@ -180,7 +180,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
