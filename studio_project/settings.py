ca14cfc75e605426e1a247d7ed243af3a363b441
Court-Pencil <courtneyhulse97@outlook.com>
2026-07-03 13:44:47 +0100

    fix: revert to StaticFilesStorage to fix static files on Heroku

--- studio_project/settings.py
+++ studio_project/settings.py
@@ -180,7 +180,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
